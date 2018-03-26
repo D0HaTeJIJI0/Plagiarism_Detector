6130c1df347c2ed1486df8714d7b3266b11f34ba
D0HaTeJIJI0 <trav_git@mail.ru>
2018-03-26 12:46:52 +0300

    Add TokenTypeJava.java

---
+++ src/lib/tokenizing/TokenTypeJava.java
@@ -0,0 +1,17 @@
+package lib.tokenizing;
+
+public enum TokenTypeJava {
+
+    ACCESS_CONTROL_MODIFIER,
+    NON_ACCESS_MODIFIER,
+    BASIC_TYPE,
+    CYCLE,
+    BIN_OP,
+    UN_OP,
+    COMPARISON_OP,
+    CONDIT,
+    LOG_OP,
+    NUMBER;
+
+
+}
