40e9b66fb8e73e030bb8c7bbbe7b60a5c399480a
D0HaTeJIJI0 <trav_git@mail.ru>
2018-04-17 15:16:21 +0300

    Add Algorithm.java

---
+++ src/alg/Algorithm.java
@@ -0,0 +1,10 @@
+package alg;
+
+import lib.tokenizing.TokenTypeJava;
+import java.util.ArrayList;
+
+public interface Algorithm {
+
+    double detect(ArrayList<TokenTypeJava> code1, ArrayList<TokenTypeJava> code2);
+
+}
